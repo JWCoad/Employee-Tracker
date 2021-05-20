3fe3052bdcd0b3e182ae73637738a0e465540524
Jesse Coad <jessecoad@gmail.com>
2021-05-20 22:32:15 +1000

    add role

--- index.js
+++ index.js
@@ -12,18 +12,15 @@ let newEmployeeCoWorkers = "";
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "",
   database: "employee_trackerDB",
 });
 
-onnection.connect(function (err) {
+connection.connect(function (err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId + "\n");
 
@@ -86,5 +83,161 @@ const start = () => {
         console.log("Quitting application...");
         connection.end();
       }
+    });
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName",
+      },
+      {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName",
+      },
+      {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "role",
+        choices: roles,
+      },
+    ])
+    .then((response) => {
+      newEmployeeFirstName = response.firstName;
+      newEmployeeLastName = response.lastName;
+
+      connection.query(
+        `SELECT role.department_id, department.name, role.id FROM role INNER JOIN department ON role.department_id = department.id WHERE (role.title = "${response.role}")`,
+        function (err, res) {
+          if (err) throw err;
+          newEmployeeRoleId = res[0].id;
+          connection.query(
+            `SELECT employee.first_name, employee.last_name, employee.id
+                FROM ((department INNER JOIN role ON role.department_id = department.id) INNER JOIN employee ON employee.role_id = role.id) WHERE department.name = "${res[0].name}"`,
+            function (err, employees) {
+              if (err) throw err;
+
+              newEmployeeCoWorkers = employees;
+
+              filteredEmployees = employees.map((employee) => {
+                return `${employee.first_name} ${employee.last_name}`;
+              });
+
+              filteredEmployees.push("None");
+
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    name: "manager",
+                    choices: filteredEmployees,
+                  },
+                ])
+                .then((response) => {
+                  if (response.manager === "None") {
+                    // console.log("Insert employee with manager id NULL");
+                    connection.query(
+                      `INSERT INTO employee SET ?`,
+                      {
+                        first_name: newEmployeeFirstName,
+                        last_name: newEmployeeLastName,
+                        role_id: newEmployeeRoleId,
+                        manager_id: null,
+                      },
+                      function (err) {
+                        if (err) throw err;
+                        console.log(
+                          `Successfully added ${newEmployeeFirstName} ${newEmployeeLastName}!`
+                        );
+                        start();
+                      }
+                    );
+                  } else {
+                    console.log(`Manager Selected: ${response.manager}`);
+
+                    newEmployeeCoWorkers.forEach((coWorker) => {
+                      // console.log(coWorker);
+                      coWorkerName = `${coWorker.first_name} ${coWorker.last_name}`;
+                      // console.log(coWorkerName);
+                      if (coWorkerName === response.manager) {
+                        connection.query(
+                          `INSERT INTO employee SET ?`,
+                          {
+                            first_name: newEmployeeFirstName,
+                            last_name: newEmployeeLastName,
+                            role_id: newEmployeeRoleId,
+                            manager_id: coWorker.id,
+                          },
+                          function (err) {
+                            if (err) throw err;
+                            console.log(
+                              `Successfully added ${newEmployeeFirstName} ${newEmployeeLastName}!`
+                            );
+                            start();
+                          }
+                        );
+                      }
+                    });
+                  }
+                });
+            }
+          );
+        }
+      );
+    });
+};
+
+// Function to add a new roll
+const addRole = () => {
+  // Asks for the new role title, section and $$$
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of the role?",
+        name: "title",
+      },
+      {
+        type: "number",
+        message: "What is the salary of the role?",
+        name: "salary",
+      },
+      {
+        type: "list",
+        message: "What department does this role belong to?",
+        choices: departments,
+        name: "department",
+      },
+    ])
+    .then((role) => {
+      // take response and add it into db
+      connection.query(
+        `SELECT id FROM department WHERE (department.name = "${role.department}")`,
+        function (err, res) {
+          if (err) throw err;
+          connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: role.title,
+              salary: role.salary,
+              department_id: res[0].id,
+            },
+            function (err) {
+              if (err) throw err;
+              console.log(`The new role of "${role.title}" has been added!`);
+              console.log(
+                `Role: ${role.title}  || Salary: ${role.salary} || Department: ${role.department}`
+              );
+              roles.push(role.title);
+              start();
+            }
+          );
+        }
+      );
     });
 };
